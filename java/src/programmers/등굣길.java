e8ff5b5ce4dd892ebf86acb009d8da81370d6bf0
tony <ksang0728@gmail.com>
2024-12-21 00:17:13 +0900

    등굣길 import 수정정

--- java/src/programmers/등굣길.java
+++ java/src/programmers/등굣길.java
@@ -1,7 +1,8 @@
 package programmers;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class 등굣길 {
 
