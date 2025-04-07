583b622d302edde8346daf70f62815690164c3e4
tony <ksang0728@gmail.com>
2025-04-08 00:30:24 +0900

    boj 1764 듣보잡

--- java/src/boj/problem1764.java
+++ java/src/boj/problem1764.java
@@ -4,63 +4,77 @@ import java.io.*;
 import java.util.*;
 
 public class problem1764 {
-   
-    static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int n, m;
-    
-    static HashMap<String, String> nList = new HashMap<>();
-    static String[] mList;
-    
-    static StringBuilder sb = new StringBuilder();
-    
-    
     /*
     n을 해시맵으로 관리
     m을 입력받고 정렬
     
     m을 순서대로 해시맵에 있는지 확인
+
+    두 집합에 중복이 없기 때문에 간단하게 처리할 수 있다.
     */
-    
-    
-  public static void main(String args[]) throws IOException {
-    
-    st = new StringTokenizer(br.readLine().trim());
-    
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-    
-    int count = 0;
-    
-    
-    for(int idx=0; idx<n; idx++){
-        String input = br.readLine();
-        
-        nList.put(input, input);
-    }
-    
-    mList = new String[m];
-    
-    for(int idx=0; idx<m; idx++){
-        String input = br.readLine();
-        
-        mList[idx] = input;
-    }
-    
-    Arrays.sort(mList);
-    
-   
-    for(int idx=0; idx<m; idx++){
-        String name= mList[idx];
-        
-        if(nList.get(name) != null){
-            count++;
-            sb.append(name).append("\n");
+
+    static int n, m;
+
+    static HashMap<String, String> nList = new HashMap<>();
+    static String[] mList;
+
+    static StringBuilder sb = new StringBuilder();
+
+    static int count;
+
+    static void inputData() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        count = 0;
+
+        st = new StringTokenizer(br.readLine().trim());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        // 듣도 못한 사람
+        // 해쉬셋에 추가
+        for (int idx = 0; idx < n; idx++) {
+            String input = br.readLine().trim();
+
+            nList.put(input, input);
+        }
+
+        // 보도 못한 사람
+        mList = new String[m];
+        for (int idx = 0; idx < m; idx++) {
+            String input = br.readLine().trim();
+
+            mList[idx] = input;
         }
     }
-    
-    System.out.println(count);
-    System.out.println(sb);
-    
-  }
+
+    static void solution() {
+
+        // 보도 못한 사람을 정렬
+        Arrays.sort(mList);
+
+        // 중복되는 인원 체크
+        for (int idx = 0; idx < m; idx++) {
+            String name = mList[idx];
+
+            if (nList.get(name) != null) {
+                count++;
+                sb.append(name).append("\n");
+            }
+        }
+
+
+    }
+
+    public static void main(String args[]) throws IOException {
+        inputData();
+
+        solution();
+
+        System.out.println(count);
+        System.out.println(sb);
+
+    }
 }
